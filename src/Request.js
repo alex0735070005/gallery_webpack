0d1d39148c67da3e9c9285ff63cf0dcc1ab4e7e9
alex0735070005 <alex0735070005@gmail.com>
2019-08-22 20:50:34 +0300

    v1

---
+++ src/Request.js
@@ -0,0 +1,30 @@
+import {
+    BASE_URL,
+    CLIENT_ID,
+} from './settings';
+
+
+class Request {
+
+    listPhotos(limit, page) {
+
+        const url = `${BASE_URL}?client_id=${CLIENT_ID}&per_page=${limit}&page=${page}`;
+
+        return fetch(url).then(response => {
+            return response.json();
+        });
+    }
+
+    likePhoto(photo, like) {
+
+        const url = `${BASE_URL}${photo.id}/like/?client_id=${CLIENT_ID}`;
+
+        return fetch(url, {
+            method: 'POST',
+        }).then(response => {
+            return response.json();
+        });
+    }
+}
+
+export default new Request();
